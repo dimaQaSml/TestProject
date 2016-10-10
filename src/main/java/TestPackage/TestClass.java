1522e65daca6dd4708da7b4e9619bf7fc0ad9f99
dmitry <ddubtsov@smedialinl.com>
2016-10-10 16:00:01 +0300

    First Commit

---
+++ src/main/java/TestPackage/TestClass.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+/**
+ * Created by dmitry on 10.10.16.
+ */
+public class TestClass {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
