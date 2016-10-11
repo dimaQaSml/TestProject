6561987a3892ed8393a563bb0edfdbfcbc1d413e
dmitry <ddubtsov@smedialinl.com>
2016-10-11 12:06:40 +0300

    test

--- src/main/java/TestPackage/TestClass.java
+++ src/main/java/TestPackage/TestClass.java
@@ -5,6 +5,6 @@ package TestPackage;
  */
 public class TestClass {
     public static void main(String[] args){
-        System.out.println("Hello World!!!!");
+        System.out.println("Hello World 111!!!!");
     }
 }
